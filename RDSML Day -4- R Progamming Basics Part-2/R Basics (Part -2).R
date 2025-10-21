f9776d488366b68e36c05e693630f0ecbf11bc8f
yazdanirazu1 <raju15.mu@gmail.com>
2025-10-21 11:40:26 +0200

    Basics
    BASics -2

---
+++ RDSML Day -4- R Progamming Basics Part-2/R Basics (Part -2).R
@@ -0,0 +1,44 @@
+# Data Types in R
+# 1.Numeric-Integer / Floating Point
+# 2. String/character - "Yazdani Razu"
+# 3. Logical 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "yazdani razu"
+print(name)
+class(name)
+c = "52"
+print(c + 9)
+print(a + 9)
+class(c)
+
+
+#logical Data type-TRUE, FALSE (T , F)
+class(a > b)
+class(b > a)
+print(b > a)
+x = 19
+y = 22-3
+print(x==y)
+# Logical Operators in R
+# && Logical Operator
+# || Logical Operator
+x != y || x == y
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * (2 / 3)
+print(z)
+print(z)
+w = (4 + 3 - 3)* 2 / 3
+print(w)
+print(w)
+y = 4 ^ 3
+print(y)
+print(y)
+
+11 / 5
+11%% 5
+11 %/% 5
